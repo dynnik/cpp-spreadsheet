0d4877e5d159eab7ee3196dba32c6100ee5c929a
dynnik <110823273+dynnik@users.noreply.github.com>
2023-06-23 14:20:11 +0300

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -1,16 +1,17 @@
 #pragma once
 
+#include <algorithm>
+#include <memory>
+#include <variant>
+#include <vector>
+
 #include "common.h"
 #include "formula.h"
 
-#include <functional>
-#include <unordered_set>
-
-class Sheet;
-
-class Cell : public CellInterface {
+class Cell : public CellInterface 
+{
 public:
-    Cell(Sheet& sheet);
+    Cell(SheetInterface& sheet);
     ~Cell();
 
     void Set(std::string text);
@@ -20,17 +21,83 @@ public:
     std::string GetText() const override;
     std::vector<Position> GetReferencedCells() const override;
 
-    bool IsReferenced() const;
+    void ClearCache();
+    bool HasCache() const;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+    class Impl 
+    {
+    public:
+        virtual ~Impl() = default;
+        virtual CellInterface::Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+        virtual std::vector<Position> GetReferencedCells() const 
+        {
+            return {};
+        }
+    };
 
+    class EmptyImpl : public Impl 
+    {
+    public:
+        EmptyImpl() = default;
+        CellInterface::Value GetValue() const override 
+        {
+            return 0.0;
+        }
+        std::string GetText() const override 
+        {
+            return {};
+        }
+    };
+    
+    class FormulaImpl : public Impl 
+    {
+    public:
+        FormulaImpl(std::string text, SheetInterface& sheet) : formula_{ParseFormula(std::move(text))}, sheet_{sheet} {}
+        CellInterface::Value GetValue() const override
+        {
+            FormulaInterface::Value val = formula_->Evaluate(sheet_);
+            if (std::holds_alternative<double>(val))
+                return std::get<double>(val);
+
+            return std::get<FormulaError>(val);
+        }
+        
+        std::string GetText() const override 
+        {
+            return '=' + formula_->GetExpression();
+        }
+
+        std::vector<Position> GetReferencedCells() const override
+        {
+            return formula_->GetReferencedCells();
+        }
+
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+        const SheetInterface& sheet_;
+    };
+
+    class TextImpl : public Impl 
+    {
+    public:
+        explicit TextImpl(std::string text) : text_{std::move(text)} {}
+        CellInterface::Value GetValue() const override 
+        {
+            return text_.length() > 0 && text_[0] == '\'' ? text_.substr(1) : text_;
+        }
+        std::string GetText() const override 
+        {
+            return text_;
+        }
+
+    private:
+        std::string text_;
+    };
+
+private:
     std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    SheetInterface& sheet_;
+    mutable std::unique_ptr<Value> cache_;
 };
